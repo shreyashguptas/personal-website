16d3433d1b146eed55073de21127cdf20addea2a
Shreyash Gupta <31446588+shreyashguptas@users.noreply.github.com>
2024-06-06 09:51:29 -0500

    fixing the Navbar

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-document.addEventListener('scroll', function() {
-    const navbar = document.getElementById('myNavbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('scroll');
-    } else {
-        navbar.classList.remove('scroll');
-    }
-});
-
 // Smooth scroll and snap
 document.documentElement.style.scrollBehavior = 'smooth';
 document.documentElement.style.scrollSnapType = 'y mandatory';
