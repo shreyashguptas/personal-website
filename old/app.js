5efb96de51b370b4fa8e93d1e6dd565e218aeb1f
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-07-13 22:43:11 -0500

    revamp of the website

--- old/app.js
+++ old/app.js
@@ -6,3 +6,10 @@ const sections = document.querySelectorAll('section');
 sections.forEach(section => {
     section.style.scrollSnapAlign = 'start';
 });
+
+// Update last modified date
+document.getElementById('last-updated').textContent = 'LAST UPDATED: ' + new Date(document.lastModified).toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric'
+});
