560f87ad167b8a05780f1a7e8c02d81155588b7e
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-09-06 13:24:22 -0500

    Removing 'My-Tech' page
    I don't like that page. Does not add any value to the personal website.

--- navbar.js
+++ navbar.js
@@ -8,10 +8,23 @@ document.addEventListener('DOMContentLoaded', function() {
             <a href="/projects/projects.html">Projects</a>
             <a href="/readings/readings.html">Readings</a>
             <a href="/blogs/blogs.html">Blog</a>
-            <a href="/my-tech/my-tech.html">My Tech</a>
             <a href="/about/about.html">About</a>
         </nav>
     `;
 
     document.querySelector('header').innerHTML = navbar;
 });
+
+function createNavbar() {
+    const nav = document.createElement('nav');
+    nav.innerHTML = `
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="projects/projects.html">Projects</a></li>
+            <li><a href="readings/readings.html">Reading</a></li>
+            <li><a href="blogs/blogs.html">Blogs</a></li>
+            <li><a href="about.html">About</a></li>
+        </ul>
+    `;
+    return nav;
+}
