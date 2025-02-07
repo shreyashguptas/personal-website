fe19ad5b8bb981b9ff0f25b61088a7ca152af554
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2025-02-07 10:58:47 -0600

    Fix Supabase image configuration in Next.js config

--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
       },
-      {
+      ...(process.env.NEXT_PUBLIC_SUPABASE_URL ? [{
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL ? new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname : '',
+        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname,
         pathname: '/storage/v1/object/public/**',
-      }
+      }] : [])
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],  // Optimize for common sizes
     imageSizes: [16, 32, 48, 64, 96, 128, 256],      // Optimize for common sizes
